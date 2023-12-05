2705cbc8f73ea4bf9cfd3ed6186073cf6e30a6f0
andi ahmad <andi.saputra@salt.co.id>
2023-12-05 17:45:51 +0700

    modify REPL and add log

--- main.go
+++ main.go
@@ -7,13 +7,23 @@ import (
 	"os/user"
 )
 
+const LOGO = `
+_________  ________   ____  __. .____       _____    _______    ________ 
+\_   ___ \ \_____  \ |    |/ _| |    |     /  _  \   \      \  /  _____/ 
+/    \  \/  /   |   \|      <   |    |    /  /_\  \  /   |   \/   \  ___ 
+\     \____/    |    \    |  \  |    |___/    |    \/    |    \    \_\  \
+ \______  /\_______  /____|__ \ |_______ \____|__  /\____|__  /\______  /
+        \/         \/        \/         \/       \/         \/        \/ 
+`
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("hello %s! this is  REPL The COKLang~\n", user.Username)
+	fmt.Printf("%s\n", LOGO)
 
+	fmt.Printf("hello %s! welcome to  REPL The COKLang v1.0.0\n", user.Username)
 	fmt.Printf("Feel free to type commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -13,14 +13,13 @@ import (
 // Konsepnya adalah sama: REPL REPL membaca input, mengirimkannya ke interpreter untuk dievaluasi, mencetak hasil/keluaran dari penerjemah dan memulai lagi. Baca, Evaluasi, Cetak, Ulangi.
 // Kita belum tahu bagaimana cara "mengevaluasi" kode sumber COK sepenuhnya.
 // Kita hanya memiliki satu bagian dari proses yang bersembunyi di balik "Eval":
-
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Println(PROMPT)
+		fmt.Printf("%s", PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -30,7 +29,6 @@ func Start(in io.Reader, out io.Writer) {
 		l := lexer.New(line)
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n", tok)
-
 		}
 	}
 }
